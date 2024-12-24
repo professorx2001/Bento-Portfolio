585df712016ba97b457457e2c91abd3aa8815270
Md Zaki Hussain <mdzakihusain@gmail.com>
2024-12-25 00:51:51 +0530

    custom font changed to Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        custom: ['customFont', 'sans-serif'],
-        customBold: ['customFontBold', 'sans-serif'],
-        customItalic: ['customFontItalic', 'sans-serif']
-      },
       boxShadow: {
         custom: "12px 12px 12px rgba(0, 0, 0, 0.1), -10px -10px 10px white",
       },
