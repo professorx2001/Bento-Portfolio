b602b6e625d4833feda8da448ced9a76db2cc890
Md Zaki Hussain <mdzakihusain@gmail.com>
2025-01-07 22:04:07 +0530

    add favicon, title & darkMode resources

--- src/script.js
+++ src/script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     projects.forEach( project => {
         const divEl = document.createElement('div')
-        divEl.classList.add('pl-3')
+        divEl.classList.add('pl-2')
         divEl.innerHTML = `
             <h3 class="text-xl leading-none font-semibold">${project.id}. <a href="${project.url}" target = "_blank">${project.name}</a></h3>
             <p class ="p-2 leading-none sm:text-sm md:text-base lg:text-lg sm:leading-none md:leading-none lg:leading-none font-extralight"">${project.desc}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         custom: "12px 12px 12px rgba(0, 0, 0, 0.1), -10px -10px 10px white",
+        customDark: "12px 12px 12px rgba(0, 0, 0, 0.5), -10px -10px 10px rgba(108, 116, 104, 0.15)"
       },
     },
   },
